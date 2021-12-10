c1ff659e913182d176d6a836f7b7030862af2657
Ale M <bomberstudios@gmail.com>
2021-12-10 16:16:40 +0100

    standalone command line util

---
+++ tool.ts
@@ -0,0 +1,13 @@
+import { mergeFiles } from './src/index.js'
+import path from 'path'
+
+const sourceFile = path.resolve(__dirname, process.argv.slice(2)[0])
+const themeFile = path.resolve(__dirname, process.argv.slice(2)[1])
+const outputFile =
+  process.argv.slice(2)[2] !== undefined
+    ? path.resolve(__dirname, process.argv.slice(2)[2])
+    : path.resolve(__dirname, `output-${Date.now()}.sketch`)
+
+mergeFiles([sourceFile, themeFile, outputFile]).then((result) => {
+  console.log(result)
+})
